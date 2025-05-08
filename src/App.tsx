42cfce16505cb8f105b598436b84bae490f71622
z-q-ying <z.qiuying@outlook.com>
2025-05-07 17:53:05 -0700

    Finish Lab1 with DOM, components, forms, and router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
+import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <>
+    <HashRouter>
       <div>
-        <h1>Welcome to Web Dev</h1>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+        </Routes>
       </div>
-    </>
-  )
+    </HashRouter>);
 }
-
-export default App
