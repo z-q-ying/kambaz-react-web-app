a3c35c6857ba59bd64f97f9466a06bcd6d837b43
z-q-ying <z.qiuying@outlook.com>
2025-06-03 12:50:26 -0700

    Update default user to null

--- src/Kambaz/Account/reducer.ts
+++ src/Kambaz/Account/reducer.ts
@@ -1,24 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const defaultTestUser = {
-  _id: "123",
-  username: "iron_man",
-  password: "stark123",
-  firstName: "Tony",
-  lastName: "Stark",
-  email: "tony@stark.com",
-  dob: "1970-05-29T00:00:00.000Z",
-  role: "FACULTY",
-  loginId: "001234561S",
-  section: "S101",
-  lastActivity: "2020-10-01",
-  totalActivity: "10:21:32",
-};
+// For dev and testing purposes
+// const defaultTestUser = {
+//   _id: "123",
+//   username: "iron_man",
+//   password: "stark123",
+//   firstName: "Tony",
+//   lastName: "Stark",
+//   email: "tony@stark.com",
+//   dob: "1970-05-29T00:00:00.000Z",
+//   role: "FACULTY",
+//   loginId: "001234561S",
+//   section: "S101",
+//   lastActivity: "2020-10-01",
+//   totalActivity: "10:21:32",
+// };
 
 const initialState = {
-  // TODO: change to null after Dev stage
-  // currentUser: null,
-  currentUser: defaultTestUser,
+  // For publish, use this profile
+  currentUser: null,
+  // For dev and testing, use this profile
+  // currentUser: defaultTestUser,
 };
 
 const accountSlice = createSlice({
