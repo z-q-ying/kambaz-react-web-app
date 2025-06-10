b8f4389d1162f432604f15b9ea98eedae8a7ebc3
z-q-ying <z.qiuying@outlook.com>
2025-06-09 23:30:38 -0700

    A5: Supporting multiple user sessions

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -2,30 +2,38 @@ import axios from "axios";
 
 import type { Credentials } from "../../types/Credentials";
 
+const axiosWithCredentials = axios.create({ withCredentials: true }); // add cookies to requests
+
 export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
 export const signin = async (credentials: Credentials) => {
-  const response = await axios.post(`${USERS_API}/signin`, credentials);
-  return response.data;
-};
-
-export const signup = async (user: any) => {
-  const response = await axios.post(`${USERS_API}/signup`, user);
-  return response.data;
-};
-
-export const updateUser = async (user: any) => {
-  const response = await axios.put(`${USERS_API}/${user._id}`, user);
+  const response = await axiosWithCredentials.post(
+    `${USERS_API}/signin`,
+    credentials
+  );
   return response.data;
 };
 
 export const profile = async () => {
-  const response = await axios.post(`${USERS_API}/profile`);
+  const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
+  return response.data;
+};
+
+export const signup = async (user: any) => {
+  const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
   return response.data;
 };
 
 export const signout = async () => {
-  const response = await axios.post(`${USERS_API}/signout`);
+  const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
+  return response.data;
+};
+
+export const updateUser = async (user: any) => {
+  const response = await axiosWithCredentials.put(
+    `${USERS_API}/${user._id}`,
+    user
+  );
   return response.data;
 };
